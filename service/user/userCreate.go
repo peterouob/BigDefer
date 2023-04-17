ab5efb389d67f55eb4d576a7781bcc0e5294901f
林庭楷 <hellolinpeter@gmail.com>
2023-04-17 16:08:12 +0800

    新增使用mysql資料庫

--- service/user/userCreate.go
+++ service/user/userCreate.go
@@ -23,6 +23,7 @@ func CreateUser(c *gin.Context) {
 	//if err := c.ShouldBind(&req); err != nil {
 	//	fmt.Println("Bind Data error", err)
 	//}
+
 	acc := model.User{
 		Name:     "peter",
 		UserName: "defer",
