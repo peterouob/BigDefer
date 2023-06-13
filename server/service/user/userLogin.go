88fb06c45fdbc2b7957305d28e7d4431cb62e3e9
peter <hellolinpeter@gmail.com>
2023-06-13 19:44:42 +0800

    新增管理員角色登入

--- server/service/user/userLogin.go
+++ server/service/user/userLogin.go
@@ -2,6 +2,7 @@ package userService
 
 import (
 	"bigdefer/orm/dal"
+	"bigdefer/orm/model"
 	H "bigdefer/service/http"
 	"bigdefer/utils"
 	"fmt"
@@ -20,35 +21,53 @@ type UserLoginResponse struct {
 	Token string `json:"token"`
 }
 
+var Userinfo chan *model.User
+
 func LoginUser(c *gin.Context) {
 	var req UserLoginRequest
 	if err := c.ShouldBind(&req); err != nil {
 		fmt.Println("Bind data error:", err)
 		return
 	}
-	acc, err := dal.User.Where(dal.User.UserName.Eq(req.UserName)).First()
-	if err != nil {
-		H.Fail(c, "Login error")
-		return
-	}
-	if len(req.PassWord) > 0 && req.PassWord == acc.Password {
+
+	if req.UserName == "main" && req.PassWord == "main" {
+		str, _ := strconv.Atoi(req.UserName)
 		token := utils.Token{
-			Uid:      strconv.Itoa(int(acc.ID)),
-			Username: acc.UserName,
+			Uid:      strconv.Itoa(str),
+			Username: req.UserName,
 			StandardClaims: jwt.StandardClaims{
 				ExpiresAt: time.Now().Add(7 * 24 * time.Hour).UnixMilli(),
 			},
 		}
-		tk, err := utils.SignToken(&token)
-		if err != nil {
-			H.Fail(c, "token error")
-			return
-		} else {
-			H.SetCookieForToken(c, "token", tk)
-			H.OK(c, "success login")
-		}
+		tk, _ := utils.SignToken(&token)
+		H.SetCookieForToken(c, "main", tk)
+		H.OK(c, "管理員你好")
 	} else {
-		H.Fail(c, "Login error")
-		return
+		acc, err := dal.User.Where(dal.User.UserName.Eq(req.UserName)).First()
+		if err != nil {
+			H.Fail(c, "Login error")
+			return
+		}
+		if len(req.PassWord) > 0 && req.PassWord == acc.Password {
+			token := utils.Token{
+				Uid:      strconv.Itoa(int(acc.ID)),
+				Username: acc.UserName,
+				StandardClaims: jwt.StandardClaims{
+					ExpiresAt: time.Now().Add(7 * 24 * time.Hour).UnixMilli(),
+				},
+			}
+			tk, err := utils.SignToken(&token)
+			if err != nil {
+				H.Fail(c, "token error")
+				return
+			} else {
+				Userinfo <- acc
+				H.SetCookieForToken(c, "token", tk)
+				H.OK(c, "success login")
+			}
+		} else {
+			H.Fail(c, "Login error")
+			return
+		}
 	}
 }
