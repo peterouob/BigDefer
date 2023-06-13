4d9d915bb17b4a572f925661b68d39469cd78d46
peter <hellolinpeter@gmail.com>
2023-06-13 19:16:34 +0800

    新增刷新token機制

--- server/middleware/auth.go
+++ server/middleware/auth.go
@@ -2,8 +2,12 @@ package middleware
 
 import (
 	H "bigdefer/service/http"
+	userService "bigdefer/service/user"
 	"bigdefer/utils"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
+	"strconv"
+	"time"
 )
 
 func Auth() gin.HandlerFunc {
@@ -17,7 +21,30 @@ func Auth() gin.HandlerFunc {
 		if err != nil {
 			H.Forbidden(c, "你沒有權限進入")
 			return
+		} else {
+			u := <-userService.Userinfo
+			t := time.Now().Add(7 * 24 * time.Hour).UnixMilli()
+			if parse.ExpiresAt-time.Now().Unix() < t {
+				token := utils.Token{
+					Uid:      strconv.Itoa(int(u.ID)),
+					Username: u.UserName,
+					StandardClaims: jwt.StandardClaims{
+						ExpiresAt: time.Now().Add(7 * 24 * time.Hour).UnixMilli(),
+					},
+				}
+				tk, err := utils.SignToken(&token)
+				if err != nil {
+					H.Forbidden(c, "continue token error")
+				} else {
+					H.SetCookieForToken(c, "new-token", tk)
+					c.Header("new-token-time", strconv.FormatInt(token.ExpiresAt, 10))
+					H.OK(c, "token updated successfully")
+				}
+			} else {
+
+			}
 		}
 		c.Set("token", parse)
+		c.Next()
 	}
 }
