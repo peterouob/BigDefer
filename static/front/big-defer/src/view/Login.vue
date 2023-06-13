49062b1944f7c7b2de397d06d8ff2e7cb2fbed97
peter <hellolinpeter@gmail.com>
2023-06-13 19:44:54 +0800

    新增管理員角色登入

--- static/front/big-defer/src/view/Login.vue
+++ static/front/big-defer/src/view/Login.vue
@@ -12,6 +12,13 @@
         <div>
           <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
           <div class="mt-2">
+            <el-input v-model="username" id="email" name="email" type="email" autocomplete="email" required="" class="block w-full rounded-md border-0  text-gray-900 shadow-sm ring-1 ring-inset placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+          </div>
+        </div>
+
+        <div v-if="username==='main' ">
+          <label for="email" class="block text-sm font-medium leading-6 text-gray-900">驗證</label>
+          <div class="mt-2">
             <el-input id="email" name="email" type="email" autocomplete="email" required="" class="block w-full rounded-md border-0  text-gray-900 shadow-sm ring-1 ring-inset placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
           </div>
         </div>
@@ -24,12 +31,12 @@
             </div>
           </div>
           <div class="mt-2">
-            <el-input id="password" name="password" type="password" autocomplete="current-password" required="" class="block w-full rounded-md border-0  text-gray-900 shadow-sm ring-1 ring-inset  placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            <el-input v-model="password" id="password" name="password" type="password" autocomplete="current-password" required="" class="block w-full rounded-md border-0  text-gray-900 shadow-sm ring-1 ring-inset  placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
           </div>
         </div>
 
         <div>
-          <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+          <button type="submit" @click="login" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
         </div>
       </form>
 
@@ -41,3 +48,13 @@
     </div>
   </div>
 </template>
+
+<script setup>
+import {ref} from 'vue'
+const username = ref("")
+const password = ref("")
+
+const login = async ()=>{
+  await axios.post
+}
+</script>
